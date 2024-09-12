016350f2d03dffa6ba28ea9723dfc719d2605298
vijo07001 <vijo07001@E-PW05CGQX>
2024-09-12 13:04:21 +0200

    Add project files.

---
+++ triangle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace triangle
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Black;
+            {
+
+            
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /\"\"\\");
+            Console.WriteLine(" /____\\");
+                Console.ReadKey();
+            }
+        }
+    }
+}
